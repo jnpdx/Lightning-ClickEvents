71a685fe60b68ee31ef40bc09079174f6a8ab92b
jnpdx <john@johnnastos.com>
2022-10-28 07:55:33 -0700

    Initial Commit

---
+++ ClickEvents/ClickEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickEventsApp.swift
+//  ClickEvents
+//
+//  Created by John Nastos on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ClickEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
